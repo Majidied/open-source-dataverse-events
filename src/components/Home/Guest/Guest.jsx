dc248f2cfd4bf57682b934040117ab38c106eaf8
Majidied <majidimajidi2003@gmail.com>
2024-10-06 15:53:40 +0100

    modifications on guest

--- src/components/Home/Guest/Guest.jsx
+++ src/components/Home/Guest/Guest.jsx
@@ -20,7 +20,7 @@ const Guest = () => {
     slidesToShow: 3, // Show 3 guests at a time
     slidesToScroll: 1, // Scroll one guest at a time
     autoplay: true, // Auto-scroll
-    autoplaySpeed: 2000, // Auto-scroll every 3 seconds
+    autoplaySpeed: 1000, // Auto-scroll every 1 seconds
     responsive: [
       {
         breakpoint: 1024,
