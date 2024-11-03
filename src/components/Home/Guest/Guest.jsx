2310d18dd8afbbdc1b1aceb2c0303b3477cbf54e
Majidied <majidimajidi2003@gmail.com>
2024-11-03 13:42:26 +0100

    Refactor Guest component to improve styling and add LinkedIn icons

--- src/components/Home/Guest/Guest.jsx
+++ src/components/Home/Guest/Guest.jsx
@@ -2,9 +2,10 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './Guest.css'; // Custom CSS
+import { FaLinkedinIn } from 'react-icons/fa';
 
 // Import images
-import image1 from './image1.jpg';
+import image1 from './image1.png';
 import image2 from './image2.jpg';
 import image3 from './image3.jpg';
 import image4 from './image4.jpg';
@@ -12,7 +13,7 @@ import image5 from './607441.png';
 
 // Data for conferenciers
 const conferenciers = [
-  { name: 'Dr. Alice Doe', title: 'Expert en Développement Durable', img: image1 },
+  { name: 'Hamza Abqar', title: 'Tech-Lead & Senior software engineer', img: image1 },
   { name: 'Prof. Bob Smith', title: 'Directeur de la Recherche', img: image2 },
   { name: 'M. Charlie Johnson', title: 'Spécialiste en Réalité Augmentée', img: image3 },
   { name: 'Mme Dana White', title: 'Designer UX', img: image4 },
@@ -82,13 +83,11 @@ const Guest = () => {
       <Slider {...settings} className="guest-slider w-full">
         {conferenciers.map((conferencier, index) => (
           <div key={index} className="guest-item shadow-lg p-6 bg-transparent card-item flex align-center">
-            <img src={conferencier.img} alt={conferencier.name} className="user-image" />
-            <h3 className="text-2xl font-semibold mb-4 user-name">{conferencier.name}</h3>
+            <img src={conferencier.img} alt={conferencier.name} className="user-image" style={{marginLeft : 'auto' , marginRight : ' auto'}} />
+            <h3 className="text-2xl text-white font-semibold mb-4 user-name">{conferencier.name}</h3>
             <p className="user-profession">{conferencier.title}</p>
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="linkedin-button">
-              <svg className="linkedin-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M20.452 20.452h-3.836v-5.573c0-1.327-.027-3.034-1.846-3.034-1.846 0-2.13 1.442-2.13 2.928v5.679h-3.836V9h3.68v1.513h.052c.513-.975 1.765-2 3.633-2 3.884 0 4.6 2.56 4.6 5.906v6.033zm-12.778-12.78c-1.19 0-2.036-.853-2.036-1.914 0-1.067.855-1.916 2.036-1.916 1.179 0 2.034.849 2.034 1.916 0 1.061-.855 1.914-2.034 1.914zm1.917 12.78h-3.834V9h3.834v11.452z" />
-              </svg>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="linkedin-button" style={{alignSelf : 'flex-end' }}>
+              <FaLinkedinIn />
             </a>
           </div>
         ))}
